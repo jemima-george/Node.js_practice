3859463cc357c8788eb661bd76420c713c570c9a
Jemima <jemima.a.george@gmail.com>
2026-08-12 09:42:26 +0400

    Argument Parsing with Yargs Challenge 2

--- Task 3/app.js
+++ Task 3/app.js
@@ -13,8 +13,21 @@ const getNotes = require("./notes.js")
 yargs.command({
     command:'add',
     description: 'Add a new note',
-    handler: function(){
-        console.log("Adding a new note")
+    builder: {
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe: 'Note body',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler: function(argv){
+        console.log("Title: " + argv.title)
+        console.log("Body: " + argv.body)
     }
 })
 
@@ -45,6 +58,6 @@ yargs.command({
     }
 })
 
-console.log(yargs.argv)
+yargs.parse()
 
 
