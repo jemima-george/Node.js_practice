4986bb2b8ea5392d7cc6ede9471c35b234a1756a
Jemima <jemima.a.george@gmail.com>
2026-08-17 17:49:19 +0400

    JSON HTTP Endpoint

--- web_server/src/app.js
+++ web_server/src/app.js
@@ -1,5 +1,6 @@
 // Path node module to get path to other files
 const path = require("path")
+require('dotenv').config();
 
 // Express npm package - used to create web servers with node that can serve json data to brower and render it to the screen
 const express = require("express")
@@ -11,6 +12,9 @@ const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public') 
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
+
+const geocode = require('../../weather_app/utils/geocode.js') 
+const forecast = require('../../weather_app/utils/forecast.js') 
 
 // Setup handlebars engine and views location
 // Hbs handlebars used to render dynamic content - set express view engine as hbs npm package 
@@ -45,9 +49,38 @@ app.get('/help', (req,res) =>{
 })
 
 app.get('/weather', (req,res) =>{
+    // Get request url and pasre query part to get address
+    if (!req.query.address){
+        return res.send({
+            error: "You must provide an address term."
+        })
+    }
+
+    geocode(req.query.address, (error, {latitude, longitude, location}) => {
+        if (error){
+            return res.send({error})
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error){
+                return res.send({error})
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+}) 
+
+app.get('/products', (req,res) =>{
+    if (!req.query.search){
+        return res.send({
+            error: "You must provide a search term"
+        })
+    }
     res.send({
-        forecast: 'The current temperature is 32 degrees.',
-        location: "Boston"
+        products: []
     })
 })
 
